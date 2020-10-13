bebe337db0549fc6143fcaa7d42343c9728eebf3
Artem <theartemka6@mail.ru>
2020-10-13 20:56:09 +0300

    Added Main

---
+++ Main.java
@@ -0,0 +1,44 @@
+package com.max.idea;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    //-----------------------------------------Integer--------------------------------------------------
+        MyList<Integer> list = new MyList<> ((a, b) -> {
+            if(((int)a.getValue() - (int)b.getValue()) > 0) {
+                return 1;
+            }
+            else if (((int)a.getValue() - (int)b.getValue()) < 0) {
+                return -1;
+            }
+            else {
+                return 0;
+            }
+        });
+        Randomizer.randomInt(list, 5);
+        System.out.println(list.toString());
+        list.sort();
+        System.out.println(list.toString());
+//-----------------------------------------Double----------------------------------------------------
+//        MyList<Double> list = new MyList<> ((a, b) -> {
+//            if(((double)a.getValue() - (double)b.getValue()) > 0) {
+//                return 1;
+//            }
+//            else if (((double)a.getValue() - (double)b.getValue()) < 0) {
+//                return -1;
+//            }
+//            else {
+//                return 0;
+//            }
+//        });
+//        Randomizer.randomDouble(list, 5);
+//        System.out.println(list.toString());
+//        list.sort();
+//        System.out.println(list.toString());
+//------------------------------------------String---------------------------------------------------
+//        MyList<String> list = new MyList<>();
+//        Randomizer.randomString(list, 5, 5);
+//        System.out.println(list.toString());
+    }
+}
